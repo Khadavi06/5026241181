275f70893412e78df2f31a5e44b8b0dbedb55d26
Khadavi06 <khadavikhalid06@gmail.com>
2026-04-22 12:13:04 +0700

    Tugas pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,40 @@ Route::get('blog', function () {
     return view('blog');
 });
 
-Route::get('pert5', function () {
+Route::get('pt5', function () {
+    return view('pertemuan5Index');
+});
+
+Route::get('pertemuan5', function () {
     return view('pertemuan5');
 });
 
+Route::get('TugasLinktreePt5', function () {
+    return view('TugasLinktreePt5');
+});
+
+Route::get('pt1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('pt2', function () {
+    return view('pertemuan2');
+});
+
+Route::get('pt3', function () {
+    return view('pertemuan3');
+});
+
+Route::get('tg3', function () {
+    return view('TugasPertemuan3');
+});
+
+Route::get('pt4', function () {
+    return view('pertemuan4');
+});
 
 Route::get('dosen', [DosenController::class, 'index']);
 
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+
